c33a59081d1d003db3278f976b1e12f99867c482
Влад Кармазин <karmazin00@gmail.com>
2022-06-10 00:30:23 +0500

    Исправлены время и дата, переписаны с учетом MVVM

--- DishPicker/View/MainWindow.xaml.cs
+++ DishPicker/View/MainWindow.xaml.cs
@@ -24,32 +24,5 @@ namespace DishPicker
         {
             InitializeComponent();
         }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            var timer = new System.Windows.Threading.DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 1, 0);
-            timer.IsEnabled = true;
-            timer.Tick += (o, e) => { txtbxTime.Text = DateTime.Now.ToString("HH:mm"); };
-            timer.Start();
-
-            txtbxTime.Text = DateTime.Now.ToString("HH:mm");
-            txtbxDate.Text = ConvertDay();
-        }
-
-        private static string ConvertDay()
-        {
-            return DateTime.Now.ToString("dddd") switch
-            {
-                "понедельник" => "ПН",
-                "вторник" => "ВТ",
-                "среда" => "СР",
-                "четверг" => "ЧТ",
-                "пятница" => "ПТ",
-                "суббота" => "СБ",
-                "воскресенье" => "ВС",
-                _ => "ХЗ",
-            };
-        }
     }
 }
--- DishPicker/ViewModel/MainViewModel.cs
+++ DishPicker/ViewModel/MainViewModel.cs
@@ -11,10 +11,47 @@ namespace DishPicker.ViewModel
     {
         public ObservableCollection<Product> Products { get; set; }
 
+        private string time;
+        private string day;
+
+        public string Time
+        {
+            get => time; 
+            set => Set(ref time, value);
+        }
+
+        public string Day
+        {
+            get => day;
+            set => Set(ref day, value);
+        }
+
         // Конструктор
         public MainViewModel() 
         {
+            var timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 1, 0);
+            timer.IsEnabled = true;
+            timer.Tick += (o, e) => { Time = DateTime.Now.ToString("HH:mm"); };
+            timer.Start();
 
+            time = DateTime.Now.ToString("HH:mm");
+            day = ConvertDay();
+        }
+
+        private static string ConvertDay()
+        {
+            return DateTime.Now.ToString("dddd") switch
+            {
+                "понедельник" => "ПН",
+                "вторник" => "ВТ",
+                "среда" => "СР",
+                "четверг" => "ЧТ",
+                "пятница" => "ПТ",
+                "суббота" => "СБ",
+                "воскресенье" => "ВС",
+                _ => "ХЗ",
+            };
         }
     }
 }
