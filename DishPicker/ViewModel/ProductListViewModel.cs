fa87cbd0005dad8ed4a62f51ee047efe0f2d6bfc
Влад Кармазин <karmazin00@gmail.com>
2022-06-14 22:06:03 +0500

    Написана заготовка интерфейса окна продуктов

---
+++ DishPicker/ViewModel/ProductListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DishPicker.ViewModel.Base;
+
+namespace DishPicker.ViewModel
+{
+    class ProductListViewModel : BaseViewModel
+    {
+
+    }
+}
